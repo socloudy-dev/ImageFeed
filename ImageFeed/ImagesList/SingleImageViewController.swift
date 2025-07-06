0fa5211adf39d1568c9f914852671f69425d06d6
socloudy-dev <vlad98.com@gmail.com>
2025-07-06 17:10:03 +0300

    Вариант с сегвеями изменен на представление изображения на весь экран с помощью модального окна. Добавлен метод центрирования изображения при его открытии изумировании. Добавлены кнопки "Back" и "Share" на экран Single Image.

--- ImageFeed/ImagesList/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -2,17 +2,97 @@ import UIKit
 
 final class SingleImageViewController: UIViewController {
     
+    //MARK: - Properties
+    
     var image: UIImage? {
         didSet {
             guard isViewLoaded else { return }
             singleImage.image = image
+            guard let unwrappedImage = singleImage.image else { return }
+            rescaleAndCenterImageInScrollView(image: unwrappedImage)
         }
     }
     
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var scrollViewOfSingleImage: UIScrollView!
+    @IBOutlet weak var backwardButton: UIButton!
     @IBOutlet private var singleImage: UIImageView!
+    
+    //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         singleImage.image = image
+        guard let unwrappedImage = singleImage.image else { return }
+        singleImage.frame.size = unwrappedImage.size
+        scrollViewOfSingleImage.minimumZoomScale = 0.1
+        scrollViewOfSingleImage.maximumZoomScale = 1.25
+    
+        rescaleAndCenterImageInScrollView(image: unwrappedImage)
+        //новое
+        scrollViewDidEndZooming(scrollViewOfSingleImage, with: singleImage, atScale: 1.25)
+    }
+    
+    //MARK: - Sethup Methods NEED TO REVIEW
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollViewOfSingleImage.minimumZoomScale
+        let maxZoomScale = scrollViewOfSingleImage.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollViewOfSingleImage.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollViewOfSingleImage.setZoomScale(scale, animated: false)
+        scrollViewOfSingleImage.layoutIfNeeded()
+        
+        updateContentInset()
+    }
+    
+    private func updateContentInset() {
+        let scrollViewSize = scrollViewOfSingleImage.bounds.size
+        let imageSize = singleImage.frame.size
+
+        let verticalInset = imageSize.height < scrollViewSize.height ?
+            (scrollViewSize.height - imageSize.height) / 2 : 0
+        let horizontalInset = imageSize.width < scrollViewSize.width ?
+            (scrollViewSize.width - imageSize.width) / 2 : 0
+
+        scrollViewOfSingleImage.contentInset = UIEdgeInsets(
+            top: verticalInset,
+            left: horizontalInset,
+            bottom: verticalInset,
+            right: horizontalInset
+        )
+    }
+    //MARK: - IBActions
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    //новое
+    @IBAction func didTapShareButton(_ sender: Any) {
+        guard let image else { return }
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+}
+
+
+//MARK: - SingleImageViewController Extensions
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return singleImage
+    }
+        //новое
+    func scrollViewDidEndZooming(_: UIScrollView, with: UIView?, atScale: CGFloat) {
+        updateContentInset()
     }
 }
